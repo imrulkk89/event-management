7a42714ba55b0f3822e05d20ff41d5d05dd69ed4
Imrul Khan <imrulkaiskhan@gmail.com>
2023-11-08 21:06:23 +0600

    config module installed and added - module, controller and service created for events, workshops and resevations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { EventsModule } from './events/events.module';
+import { WorkshopsModule } from './workshops/workshops.module';
+import { ReservationsModule } from './reservations/reservations.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    EventsModule,
+    WorkshopsModule,
+    ReservationsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
